7a95bdfc9672b496b18db6e4142f252b413b9144
w4n9r3ntru3 <patrick1031wang@gmail.com>
2020-10-15 09:01:18 +0800

    :robot: W4n9RenTru3Bot auto commit at 2020-10-15-09-01-18

--- golang_test/interface_test/interface.go
+++ golang_test/interface_test/interface.go
@@ -10,13 +10,21 @@ type Fooer interface {
 type Foo struct {
 	Field int
 }
+
 // Please refer to https://stackoverflow.com/a/44372954
 func (f *Foo) Dummy() {}
 func (f *Foo) Add()   { f.Field += 1 }
 
+func willFail(fr *Fooer) {}
+
 func main() {
 	var f1 Foo
 	var f2 *Foo = &Foo{Field: 3}
+
+	// willFail(&f1)
+
+	// I'm just using f1 as demonstration
+	_ = f1
 
 	// ! Error happens here. Refer to the stackoverflow link above
 	// DoFoo(f1)
