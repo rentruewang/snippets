4b2ddf6a27a900099d0b7df4920de6900fccc651
w4n9r3ntru3 <patrick1031wang@gmail.com>
2020-10-23 12:40:47 +0800

    :truck: Routes are moved to a new repo

--- route/target/debug/deps/route-f41d60cd05ce59d5.d
+++
@@ -1,5 +0,0 @@
-/Users/forerunner/Git/doing-now/route/target/debug/deps/route-f41d60cd05ce59d5.rmeta: src/main.rs
-
-/Users/forerunner/Git/doing-now/route/target/debug/deps/route-f41d60cd05ce59d5.d: src/main.rs
-
-src/main.rs:
