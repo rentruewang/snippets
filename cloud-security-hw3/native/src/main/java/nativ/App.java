e91f3019db7d1503744198ed1d66e0c14011f4ee
RenChu Wang <patrick1031wang@gmail.com>
2020-11-02 05:00:01 +0800

    :robot: W4n9RenTru3Bot auto commit at 2020-11-02-05-00-01

--- cloud-security-hw3/native/src/main/java/nativ/App.java
+++ cloud-security-hw3/native/src/main/java/nativ/App.java
@@ -1,13 +1,9 @@
 package nativ;
 
-import java.io.IOException;
-import java.util.StringTokenizer;
-import java.util.regex.*;
-import java.util.stream.*;
-import java.util.*;
-import java.util.function.*;
-
-import com.google.common.collect.Lists;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map.Entry;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -18,46 +14,56 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-// FIXME
 class Main {
+    static final String date = "Mar/2004:";
 }
 
 class MyMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
 
-    private final Text key = new Text();
-    private final Text val = new Text();
-
-    private static final String date = "Mar/2004:";
-
-    public void map(LongWritable lineCount, Text line, Context context) throws IOException, InterruptedException {
-        var lineStr = line.toString();
-        var index = lineStr.indexOf(date) + date.length();
+    @Override
+    public void map(LongWritable _lc, Text line, Context context) {
+        var lineStr = line.toString().trim();
+        var index = lineStr.indexOf(Main.date) + Main.date.length();
 
         try {
             var hour = Integer.parseInt(lineStr.substring(index, index + 2));
             var writable = new IntWritable(hour);
             context.write(writable, line);
         } catch (Exception e) {
-            System.out.println(e);
+            System.out.println("Mapper: " + e.toString());
+            System.exit(1);
         }
     }
 }
 
 class MyReducer extends Reducer<IntWritable, Text, Text, IntWritable> {
-    private static final String date = "Mar/2004:";
-
     private static String reduced(String input) {
-        var index = input.indexOf(date);
-        return input.substring(index + date.length());
+        var index = input.indexOf(Main.date);
+        return input.substring(index, index + Main.date.length() + 2) + ":00:00";
     }
 
-    public void reduce(IntWritable key, Iterable<Text> values, Context context) {
-        var mapped = Lists.newArrayList(values).stream().map(Text::toString).reduce(
-            new HashMap<String, ArrayList<String>>(), (map,value) ->  {
-                var re = MyReducer::reduced(value);
-                map.
-            }
-        );
+    private static void ctxWrite(Entry<String, HashSet<String>> et, Context context) {
+        try {
+            var str = new Text(et.getKey());
+            var cnt = new IntWritable(et.getValue().size());
+            context.write(str, cnt);
+        } catch (Exception ex) {
+            System.out.println("Reducer: " + ex);
+            System.exit(1);
+        }
+    }
+
+    @Override
+    public void reduce(IntWritable _k, Iterable<Text> values, Context context) {
+        var map = new HashMap<String, HashSet<String>>();
+        for (var log : values) {
+            var logStr = log.toString().trim();
+            var set = map.computeIfAbsent(MyReducer.reduced(logStr), s -> new HashSet<String>());
+            set.add(logStr);
+        }
+        for (var entry : map.entrySet()) {
+            MyReducer.ctxWrite(entry, context);
+        }
 
     }
 }
@@ -68,14 +74,16 @@ public class App {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "word count");
-        job.setJarByClass(WordCount.class);
-        job.setMapperClass(TokenizerMapper.class);
-        // job.setCombinerClass(IntSumReducer.class);
-        // job.setReducerClass(IntSumReducer.class);
-        // job.setOutputKeyClass(Text.class);
-        // job.setOutputValueClass(IntWritable.class);
-        // FileInputFormat.addInputPath(job, new Path(args[0]));
-        // FileOutputFormat.setOutputPath(job, new Path(args[1]));
-        // System.exit(job.waitForCompletion(true) ? 0 : 1);
+        job.setJarByClass(Main.class);
+        job.setMapperClass(MyMapper.class);
+        job.setCombinerClass(Reducer.class);
+        job.setReducerClass(MyReducer.class);
+        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
--- cloud-security-hw3/streaming/reducer.py
+++ cloud-security-hw3/streaming/reducer.py
@@ -1,11 +1,11 @@
 import sys
 from collections import defaultdict
 
-groups = defaultdict(int)
+groups = defaultdict(set)
 
 for line in sys.stdin:
-    (key, _) = line.strip().split("\t", maxsplit=1)
-    groups[key] += 1
+    (key, val) = line.strip().split("\t", maxsplit=1)
+    groups[key].add(val)
 
-for (key, count) in groups.items():
-    print("{}\t{}".format(key, count), file=sys.stdout)
+for (key, val) in groups.items():
+    print("{}\t{}".format(key, len(val)), file=sys.stdout)
