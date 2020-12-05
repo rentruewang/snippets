b7cbbf7da7b72da049c3eb3c842bf8f35d9bb1c8
RenChu Wang <patrick1031wang@gmail.com>
2020-12-05 17:12:40 +0800

    🗺️ layout of the app

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -11,47 +11,45 @@ class Home extends StatelessWidget {
       appBar: AppBar(
           title: Text('my first app'),
           centerTitle: true,
-          backgroundColor: Colors.red),
-      body: Center(
-        child: IconButton(
-          onPressed: () => print('wash your hands'),
-          icon: Icon(Icons.soap),
-          color: Colors.amber,
-        ),
+          backgroundColor: Colors.red[600]),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // what is baseline?
+        crossAxisAlignment: CrossAxisAlignment.baseline,
+        textBaseline: TextBaseline.ideographic,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              FlatButton(
+                onPressed: () {},
+                color: Colors.amber,
+                child: Text('c3'),
+              ),
+              FlatButton(
+                onPressed: () {},
+                color: Colors.purple,
+                child: Text('c2'),
+              )
+            ],
+          ),
+          FlatButton(
+            onPressed: () {},
+            color: Colors.amber,
+            child: Text('click me'),
+          ),
+          Container(
+              color: Colors.cyan,
+              padding: EdgeInsets.all(1.0),
+              child: Text('inside')),
+        ],
       ),
-      floatingActionButton: IconButton(
-        onPressed: () => print('bottom right'),
-        icon: Icon(Icons.zoom_in, color: Colors.blue),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[600],
+        child: Text('click'),
+        onPressed: () {},
       ),
     );
   }
 }
-
-// snippets for icons and buttons
-
-//  Icon(
-//    Icons.airport_shuttle,
-//    color: Colors.lightBlue,
-//    size: 50.0
-//  ),
-
-//  RaisedButton(
-//    onPressed: () {
-//      print('you clicked me');
-//    },
-//    child: Text('click me'),
-//    color: Colors.lightBlue,
-//  ),
-
-//  FlatButton(
-//    onPressed: () {},
-//    child: Text('click me again'),
-//    color: Colors.amber
-//  ),
-
-//  RaisedButton.icon(
-//    onPressed: () {},
-//    icon: Icon(Icons.mail),
-//    label: Text('mail me'),
-//    color: Colors.amber,
-//  ),
