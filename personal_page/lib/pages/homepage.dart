0a5d9bdd426e8313f77d614448caf986753f7ab4
w4n9r3ntru3 <patrick1031wang@gmail.com>
2020-12-30 00:56:41 +0800

    📄 Personal page deadline approaching

---
+++ personal_page/lib/pages/homepage.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Think about what I'm going to put in here
+    var appbar = AppBar();
+    var content = ListView();
+    return Scaffold(appBar: appbar, body: content);
+  }
+}
