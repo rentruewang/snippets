8c5d55bd25454e6fcd3a71f24a067e21eae9db20
RenChu Wang <patrick1031wang@gmail.com>
2022-03-29 23:46:26 +0800

    🐍 MRO resolution order, name mangling.
    Today I learnt a lot more about python.

---
+++ python/src/mangle.py
@@ -0,0 +1,22 @@
+__something = "something"
+
+
+class A:
+    __x = "x"
+
+    def __init__(self):
+        print(self.__x)
+
+        # This is also mangled.
+        print(__something)
+
+
+class B:
+    def __init__(self):
+        self.__stupid = "stupid"
+        print(self.__stupid)
+
+
+if __name__ == "__main__":
+    B()
+    A()
---
+++ python/src/super.py
@@ -0,0 +1,67 @@
+class CoopFoo:
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)  # forwards all unused arguments
+        self.foo = "foo"
+        print(kwargs)
+
+    def f(self):
+        super().f()
+        print("coopfoo")
+
+
+class CoopBar:
+    def __init__(self, bar, **kwargs):
+        super().__init__(**kwargs)  # forwards all unused arguments
+        self.bar = bar
+        print(kwargs)
+
+    def f(self):
+        print("coopbar")
+
+
+class CoopFooBar(CoopFoo, CoopBar):
+    def __init__(self, bar="bar"):
+        super().__init__(bar=bar)
+
+    def f(self):
+        super().f()
+        print("cfb f")
+
+
+class First(object):
+    def __init__(self):
+        super().__init__()
+        print("first")
+
+    def some(self):
+        print("s first")
+
+
+class Second(object):
+    def __init__(self):
+        super().__init__()
+        print("second")
+
+    def some(self):
+        print("s second")
+
+
+class Third(First, Second):
+    def __init__(self):
+        super().__init__()
+        print("third")
+
+    def some(self):
+        super().some()
+        print("s third")
+
+
+if __name__ == "__main__":
+    cfb = CoopFooBar()
+    cfb.f()
+
+    print()
+    print()
+
+    th = Third()
+    th.some()
