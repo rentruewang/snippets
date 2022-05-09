d56a33c147ae8008366048ac8f74d8b352c2ea74
RenChu Wang <patrick1031wang@gmail.com>
2022-05-10 02:08:24 +0800

    ⚠️ Go 2 error handling.
    It's in errors package already!

---
+++ go/errors/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"os"
+)
+
+func errorsAs() {
+	if _, err := os.Open("non-existing"); err != nil {
+		var pathError *fs.PathError
+		if errors.As(err, &pathError) {
+			fmt.Println("Failed at path:", pathError.Path)
+		} else {
+			fmt.Println(err)
+		}
+	}
+}
+
+func errorsIs() {
+	if _, err := os.Open("non-existing"); err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			fmt.Println("file does not exist")
+		} else {
+			fmt.Println(err)
+		}
+	}
+}
+
+func main() {
+	errorsAs()
+	errorsIs()
+}
