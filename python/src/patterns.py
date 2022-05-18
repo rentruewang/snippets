4cefa4a1727135ca20e2d4c8b624ed9b1298e12e
RenChu Wang <patrick1031wang@gmail.com>
2022-05-19 00:09:08 +0800

    🆙 Updated python match case.
    Now including more advanced patterns like match args etc.

--- python/src/patterns.py
+++ python/src/patterns.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import Any, List, Sequence
 
 
@@ -34,6 +35,41 @@ def match_end(seq: Sequence[int]):
             print("bye")
 
 
+def alarm(item: List[str]):
+    match item:
+        case ["evening", action]:
+            print(f"It's late! Go {action} now")
+        case [("morning" | "afternoon") as time, action]:
+            print(f"Good {time}! It is time to {action}!")
+        case _:
+            print("The time is invalid.")
+
+
+class Something:
+    name: str
+    count: int
+
+    __match_args__ = ("name", "count")
+
+    def __init__(self, name: str, count: int) -> None:
+        self.name = name
+        self.count = count
+
+    def do_match(self) -> None:
+        match self:
+            case Something("sth", cnt):
+                print(f"This is sth, {cnt}")
+            case Something("other"):
+                print(f"This is other, {self}")
+            case Something(name=sth, count=8):
+                print(f"Counted {sth} eight times.")
+            case _:
+                print(self)
+
+    def __str__(self) -> str:
+        return str(self.__dict__)
+
+
 if __name__ == "__main__":
     match_something("string")
     match_something("other")
@@ -48,3 +84,12 @@ if __name__ == "__main__":
     match_end(list(range(3)))
     match_end(tuple(range(3)))
     print()
+
+    alarm(["morning", "wake up"])
+    alarm(["evening", "sleep"])
+    print()
+
+    Something("sth", 789).do_match()
+    Something("other", 666).do_match()
+    Something("hello", 8).do_match()
+    Something("default", 448).do_match()
