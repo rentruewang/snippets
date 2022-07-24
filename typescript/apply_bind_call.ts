890f77eb976605c39021801f81f6adaa6b3925cc
RenChu Wang <patrick1031wang@gmail.com>
2022-07-25 00:19:59 +0800

    🩹 Javascript functions apply, bind, and call.
    It's all because of JS's this keyword.

---
+++ typescript/apply_bind_call.ts
@@ -0,0 +1,34 @@
+type SomePerson = {
+  name: string;
+  hello: (thing: number) => void;
+};
+
+class SomePersonClass {
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  hello(thing: number): void {
+    console.log(this.name + " : " + thing);
+  }
+}
+
+let person: SomePersonClass = {
+  name: "class",
+  hello: function (thing: number) {
+    console.log(this.name + " : " + thing);
+  },
+};
+
+function use_person(person: SomePerson) {
+  person.hello(1);
+  person.hello.call({ name: "call" }, 2);
+  person.hello.apply({ name: "apply" }, [3]);
+
+  let another = person.hello.bind({ name: "bind" });
+  another(4);
+}
+
+use_person(person);
