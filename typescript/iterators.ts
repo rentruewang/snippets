13bfa0f02893d41c74b1a54802158ea0f884233e
RenChu Wang <patrick1031wang@gmail.com>
2022-10-08 13:39:30 -0400

    🆙 Updated iterator of javascript/typescript.
    Have been using quite a bit of javascript and it kindof suprised me when I constantly run into it.

---
+++ typescript/iterators.ts
@@ -0,0 +1,9 @@
+const container: number[] = [5, 4, 3, 2, 1];
+console.log(container.keys());
+console.log(container.values());
+console.log(container.entries());
+
+const object: { first: number; second: number } = { first: 1, second: 2 };
+console.log(Object.keys(object));
+console.log(Object.values(object));
+console.log(Object.entries(object));
