0e1470c3271274b77c92b62935298ec8520c1874
RenChu Wang <patrick1031wang@gmail.com>
2022-10-27 13:47:52 -0400

    😋 Apparantly copying a member array also copies its value.
    And array assignment itself is not possible.

---
+++ c/copy.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+typedef struct Array {
+    int array[4];
+} Array;
+
+int main() {
+    Array a = {.array = {1, 2, 3, 42}};
+    for (int i = 0; i < 4; ++i) {
+        printf("%d ", a.array[i]);
+    }
+    printf("\n");
+
+    Array b = a;
+    b.array[2] -= 100;
+    for (int i = 0; i < 4; ++i) {
+        printf("%d ", a.array[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 4; ++i) {
+        printf("%d ", b.array[i]);
+    }
+    printf("\n");
+
+    b = a;
+    for (int i = 0; i < 4; ++i) {
+        printf("%d ", b.array[i]);
+    }
+    printf("\n");
+
+    // https://stackoverflow.com/questions/32313150/array-type-char-is-not-assignable
+    // array type 'int[3]' is not assignable
+    //
+    // int c[3] = {3, 242, 1}, d[3];
+    // d = c;
+    // for (int i = 0; i < 3; ++i) {
+    //     printf("%d ", d[i]);
+    // }
+    // printf("\n");
+}
