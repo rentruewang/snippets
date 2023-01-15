e75e31838012a1cc3e90993b69baf69a93b5d605
RenChu Wang <patrick1031wang@gmail.com>
2023-01-15 11:38:14 -0500

    😮 Apparantly go uses a common mutable variable for range for loops as well.
    So loop closure will still be problematic even if I use range for loops. Will need to allocate a new variable per loop to avoid the problem.

--- go/loops/main.go
+++ go/loops/main.go
@@ -11,7 +11,7 @@ func main() {
 	// This might not print 0...9
 	// for i := 0; i < 10; i++ {
 	// 	go func() {
-	// 		// 	// loop variable i captured by func literal loopclosure
+	// 		// loop variable i captured by func literal loopclosure
 	// 		fmt.Println(i)
 	// 	}()
 	// }
@@ -26,4 +26,17 @@ func main() {
 	}
 
 	wg.Wait()
+
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	// for _, val := range numbers {
+	// 	wg.Add(1)
+	// 	// loop variable i captured by func literal loopclosure
+	// 	go func() { fmt.Println(val) }()
+	// }
+
+	for _, val := range numbers {
+		val := val
+		wg.Add(1)
+		go func() { fmt.Println(val) }()
+	}
 }
