ff197ef2932698c9ebcaf55ded0a1800f092d519
RenChu Wang <patrick1031wang@gmail.com>
2023-12-23 17:44:17 -0500

    🪦 Protocols would have to be explicitly subclassed to inherit default implementations.
    Per PEP 544.

---
+++ python/src/proto-auto-methods.py
@@ -0,0 +1,34 @@
+import abc
+from typing import Protocol
+
+# https://peps.python.org/pep-0544/#explicitly-declaring-implementation
+
+
+class SomeDefaults(Protocol):
+    @abc.abstractmethod
+    def function(self):
+        pass
+
+    def call_function(self):
+        print("calling call_function")
+        self.function()
+
+
+class HasFunction:
+    def function(self):
+        print("calling function")
+
+
+class Inherited(HasFunction, SomeDefaults):
+    pass
+
+
+def call_defaults(sd: SomeDefaults):
+    sd.call_function()
+
+
+if __name__ == "__main__":
+    # This doesn't work because call_function is not present on HasFunction
+    # call_defaults(HasFunction())
+
+    call_defaults(Inherited())
