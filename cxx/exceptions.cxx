eb589cb03f45712c464a8a3a92ceee514876020b
RenChu Wang <patrick1031wang@gmail.com>
2024-05-27 21:56:44 -0400

    🫴 C++ exceptions.
    It's super flexible.

---
+++ cxx/exceptions.cxx
@@ -0,0 +1,17 @@
+// Copyright (c) 2024 RenChu Wang - All Rights Reserved
+
+#include <iostream>
+#include <string>
+/// https://stackoverflow.com/questions/315948/c-catching-all-exceptions
+int main() {
+    try {
+        throw std::string("anything would be caught");
+    } catch (const std::string& str) {
+        std::cout << "Caught string: " << str << '\n';
+    } catch (...) {
+        std::cout << "Default pattern\n";
+    }
+    std::cout << "done\n";
+
+    return 0;
+}
