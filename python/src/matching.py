6de446d003654405dd3a3364c842d1475c8a972a
RenChu Wang <patrick1031wang@gmail.com>
2024-07-07 21:33:52 -0400

    🔥 `match` for python added.
    Suprisingly, `__match_args__` can be a property.

---
+++ python/src/matching.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2024 RenChu Wang - All Rights Reserved
+
+import dataclasses as dcls
+
+
+@dcls.dataclass(frozen=True)
+class DataClass:
+    name: str
+    value: int
+
+
+class MissingMatcher:
+    __match_args__ = "hello", "world"
+
+    @property
+    def hello(self):
+        return "hello"
+
+
+class CustomMatcher:
+    @property
+    def __match_args__(self):
+        # Property is ok as well.
+        return "hello", "world"
+
+    @property
+    def hello(self):
+        return "hello"
+
+    @property
+    def world(self):
+        return "world"
+
+
+if __name__ == "__main__":
+    dc = DataClass(name="name", value=3)
+
+    match dc:
+        case DataClass(name=n, value=v):
+            print(n, v)
+        case _:
+            raise ValueError
+
+    # mm = MissingMatcher()
+
+    # match mm:
+    #     # Not matching
+    #     case MissingMatcher(hello=h, world=w):
+    #         print(h, w)
+    #     case _:
+    #         raise ValueError
+
+    cm = CustomMatcher()
+
+    match cm:
+        case CustomMatcher(hello=h, world=w):
+            print(h, w)
+        case _:
+            raise ValueError
