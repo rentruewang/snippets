38fbf14b1280733a6ac6ccb8d3f1c7e58201cdee
RenChu Wang <patrick1031wang@gmail.com>
2024-07-21 21:03:26 -0700

    ⏩ Forward reference in type bound works.
    This particular pattern can be useful in node type declaration.

---
+++ python/src/self.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2024 RenChu Wang - All Rights Reserved
+
+import abc
+from collections.abc import Sequence
+from typing import Protocol, TypeVar
+
+T = TypeVar("T", bound="HasChildren", covariant=True)
+
+
+class HasChildren(Protocol[T]):
+    @abc.abstractmethod
+    def children(self) -> Sequence[T]: ...
+
+
+class HasSelfAsChildren(HasChildren["HasSelfAsChildren"]):
+    # It does autocomplete. Seems fine.
+    def children(self) -> Sequence["HasSelfAsChildren"]:
+        return []
