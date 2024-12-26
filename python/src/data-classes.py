d193ed390f73ca60caab5a05fee06a0afe819c96
RenChu Wang <patrick1031wang@gmail.com>
2024-12-25 19:05:21 -0800

    🧑‍🏫 Experiments on `dataclass`es.
    Python's `dataclass` uses type hints to determine if a field is a member or class variable, and python's classes uses `builtin_function_or_method` vs `types.FunctionType` to tell if a `Callable` would be a method would be bound to the class.

--- python/src/data-classes.py
+++ python/src/data-classes.py
@@ -1,6 +1,7 @@
 # Copyright (c) RenChu Wang - All Rights Reserved
 
 import dataclasses as dcls
+from collections.abc import Callable
 
 
 @dcls.dataclass(frozen=True)
@@ -17,6 +18,15 @@ class Sub2(Base):
     sub: str
 
 
+@dcls.dataclass(frozen=True)
+class HasMethods:
+    field: int = 3
+
+    e = lambda self: print(self)
+    f = print
+    g: Callable = lambda self: print(self)
+
+
 if __name__ == "__main__":
     s1 = Sub1(based="based", sub="sub")
     print(s1)
@@ -24,3 +34,8 @@ if __name__ == "__main__":
     # Not a dataclass, so that the fields are not overwritten.
     s2 = Sub2(based="based")
     print(s2)
+
+    print(HasMethods(3))
+    print(HasMethods(2, None))
+    HasMethods(2, None).e()
+    print(type(print))
