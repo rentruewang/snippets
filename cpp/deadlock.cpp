9e0a3099d15ea6e6c4ef90b0351d9eea891f4ad2
RenChu Wang <patrick1031wang@gmail.com>
2025-09-19 19:34:12 -0700

    🪙 Refine the code for deadlocks further.

--- cpp/deadlock.cpp
+++ cpp/deadlock.cpp
@@ -9,6 +9,7 @@
 #include <numeric>
 #include <semaphore>
 #include <sstream>
+#include <stdexcept>
 #include <thread>
 #include <vector>
 
@@ -19,14 +20,14 @@ class compute : enable_shared_from_this<compute> {
    public:
     compute() : call_count_(0) {}
     virtual ~compute() {}
-    virtual int get() = 0;
+    virtual int eval() = 0;
     virtual vector<shared_ptr<compute>> children() const = 0;
     virtual string str() const = 0;
 
     int operator()() {
         ++call_count_;
         cout << this->str() << "\n";
-        return get();
+        return eval();
     }
 
     size_t cnt() const { return call_count_; }
@@ -57,7 +58,7 @@ class sema {
    protected:
     // Using a reference s.t. the semaphore don't get copied.
     sema(counting_semaphore<>& sem) : sem_(sem) {}
-    scoped_semaphore acquire_semaphore(string by) {
+    scoped_semaphore acquire_semaphore(const string& by) {
         // Using copy elision, to avoid acquiring and releasing and acquiring.
         return scoped_semaphore(sem_, by);
     }
@@ -69,7 +70,7 @@ class sema {
 class literal : public compute {
    public:
     literal(int i) : data_(i) {}
-    int get() override { return data_; }
+    int eval() override { return data_; }
     vector<shared_ptr<compute>> children() const override { return {}; }
     string str() const override {
         stringstream out;
@@ -85,7 +86,7 @@ class summation : public compute {
    public:
     summation(vector<shared_ptr<compute>> op) : operands_(op) {}
 
-    int get() override {
+    int eval() override {
         int summation = 0;
         for (auto op : operands_) {
             summation += (*op)();
@@ -114,7 +115,7 @@ class product : public compute {
    public:
     product(vector<shared_ptr<compute>> op) : operands_(op) {}
 
-    int get() override {
+    int eval() override {
         int product = 1;
         for (auto op : operands_) {
             product *= (*op)();
@@ -143,7 +144,7 @@ class cache : public compute {
    public:
     cache(shared_ptr<compute> op) : operand(op) {}
 
-    int get() override {
+    int eval() override {
         if (value_ >= 0) {
             return value_;
         }
@@ -176,9 +177,9 @@ class task_literal : public literal, sema {
    public:
     task_literal(int i, counting_semaphore<>& sem) : literal(i), sema(sem) {}
 
-    int get() override {
+    int eval() override {
         auto sem{acquire_semaphore("task_lit_" + str())};
-        return literal::get();
+        return literal::eval();
     }
 };
 class task_summation : public summation, sema {
@@ -186,9 +187,9 @@ class task_summation : public summation, sema {
     task_summation(vector<shared_ptr<compute>> op, counting_semaphore<>& sem)
         : summation(op), sema(sem) {}
 
-    int get() override {
+    int eval() override {
         auto sem{acquire_semaphore("task_sum_" + str())};
-        return summation::get();
+        return summation::eval();
     }
 };
 
@@ -198,20 +199,20 @@ class task_summation : public summation, sema {
 class lazy_literal : public literal, sema {
    public:
     lazy_literal(int i, counting_semaphore<>& sem) : literal(i), sema(sem) {}
-    int get() override {
+    int eval() override {
         // As literal has no children, this can be the same as `literal_task`.
         auto sem{acquire_semaphore("lazy_lit_" + str())};
-        return literal::get();
+        return literal::eval();
     }
 };
 class lazy_summation : public summation, sema {
    public:
     lazy_summation(vector<shared_ptr<compute>> op, counting_semaphore<>& sem)
         : summation(op), sema(sem) {}
-    int get() override {
+    int eval() override {
         vector<int> values;
 
-// Uisng omp parallel to simulate multiple threads.
+// Using omp parallel to simulate multiple threads.
 #pragma omp parallel for
         for (auto child : children()) {
 #pragma omp critical
@@ -230,14 +231,13 @@ class lazy_summation : public summation, sema {
 void raise_error_on_deadlock(counting_semaphore<>& sem) {
     for (;;) {
         if (!sem.try_acquire()) {
-            cout << "--- deadlock! ---\n";
-            abort();
+            throw runtime_error("--- deadlock! ---");
         }
 
         // Lock is acquired.
         cout << "Acquired in deadlock detection. No deadlock\n";
         sem.release();
-        this_thread::sleep_for(chrono::seconds(3));
+        this_thread::sleep_for(chrono::seconds(1));
     }
 }
 
@@ -252,6 +252,7 @@ class scoped_deadlock_detection {
     counting_semaphore<>& sem_;
     thread th_;
 };
+
 int main() {
     using expr = shared_ptr<compute>;
     expr one, two, three, sum_six, prod_six, twelve, thrity_six;
